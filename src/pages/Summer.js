db8e1535fbddc24fab151d7523515cb091eb519c
Cup of Coding <anne@cupofcoding.com>
2022-07-11 20:35:37 -0700

    Updates

--- src/pages/Summer.js
+++
@@ -1,8 +0,0 @@
-export default function Summer() {
-    return (
-    <div className="summer-page season-page">
-        <h1>Summer</h1>
-        <img src="https://images.unsplash.com/photo-1487349384428-12b47aca925e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="Sai Kiran Anagani @anagani_saikiran https://unsplash.com/photos/83zRhEhFMfo"/>
-    </div>
-    )
-}
