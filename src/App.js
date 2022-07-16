f783c2c5e7ffc0c2648205bfaf806078f0b3c9b9
Cup of Coding <anne@cupofcoding.com>
2022-07-15 22:25:00 -0700

    changed base page to use react router dom

--- src/App.js
+++ src/App.js
@@ -4,31 +4,44 @@ import Spring from "./pages/Spring";
 import Summer from "./pages/Summer";
 import Autumn from "./pages/Autumn";
 import Winter from "./pages/Winter";
+import { Route, Routes } from 'react-router-dom';
+//these two components allow us to define all of our routes and group them together
 
 function App() {
-  let component
-  // eslint-disable-next-line default-case
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home />
-      break
-    case "/spring":
-      component = <Spring />
-      break
-    case "/summer":
-      component = <Summer />
-      break
-    case "/autumn":
-      component = <Autumn />
-      break
-    case "/winter":
-      component = <Winter />
-      break        
-  }
+  // let component
+  // // eslint-disable-next-line default-case
+  // switch (window.location.pathname) {
+  //   case "/":
+  //     component = <Home />
+  //     break
+  //   case "/spring":
+  //     component = <Spring />
+  //     break
+  //   case "/summer":
+  //     component = <Summer />
+  //     break
+  //   case "/autumn":
+  //     component = <Autumn />
+  //     break
+  //   case "/winter":
+  //     component = <Winter />
+  //     break        
+  // }
   return (
   <>
     <Navbar />
-    <div className="container">{ component }</div>
+    <div className="container">
+      {/* { component } -- used with switch statement */}
+      {/* Routes provides a list of different routes, function chooses the one that fits best and define each one as a component */}
+      <Routes>
+        {/* Each route component contains a path and element that selects what it's rendering out. It functions the same as the switch statement with some additional functions*/}
+        <Route path="/" element={<Home/>} />
+        <Route path="/spring" element={<Spring />} />
+        <Route path="/summer" element={<Summer/>} />
+        <Route path="/autumn" element={<Autumn/>} />
+        <Route path="/winter" element={<Winter/>} />
+      </Routes>
+    </div>
   </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+//BrowserRouter component imported and wrapped around the app to handle and manage all of the routing. Define the route and all the routes inside of it in the app JS. 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
