038ce6019175057ea9382de1340f2924d77a2b9d
Cup of Coding <anne@cupofcoding.com>
2022-07-26 15:35:44 -0700

    added seasons menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,14 @@ import './menu.css';
 import React, { useState } from 'react';
 // import { ReactComponent as ChevDown } from '../icons/chevron-down-solid.svg';
 import { ReactComponent as ChevRight } from '../icons/chevron-right-solid.svg';
-import { ReactComponent as TreeIcon } from '../icons/tree-solid.svg';
+import { ReactComponent as ChevLeft } from '../icons/chevron-left-solid.svg';
+
+import { ReactComponent as SeasonsIcon } from '../icons/wind-solid.svg';
+import { ReactComponent as SpringSeedling } from '../icons/seedling-solid.svg';
+import { ReactComponent as SummerUmbrella } from '../icons/umbrella-beach-solid.svg';
+import { ReactComponent as FallLeaf } from '../icons/canadian-maple-leaf-brands.svg';
+import { ReactComponent as WinterSnowflake } from '../icons/snowflake-solid.svg';
+// import { ReactComponent as XmasTreeIcon } from '../icons/tree-solid.svg';
 // transition component to control appear, enter, exit states to control which menus are visible and rendering multiple menus being added or removed from the app
 import { CSSTransition } from 'react-transition-group';
 
@@ -43,11 +50,20 @@ function DropdownMenu() {
   // DropdownMenu's can have multiple items, nested component for simplicity 
   // set current state then change state for menus. Main menu is Default and parent; Wrapping the dropdown components in a CSS transition component to animate the content.
   const [activeMenu, setActiveMenu] = useState('main') //
+  // Dynamic way to manage height for when Old menu still shows up on the DOM using useState and calculating height with a DOM element as its argument since there is a property to offset height.
+  const [menuHeight, setMenuHeight] = useState(null);
+
+  function calcHeight(ele) {
+    // offsetHeight contains the actual height and pixels of the element
+    const height = ele.offsetHeight;
+    // CSS transition has lifecycle hooks that provide information on enter and intiates a callback when onEnter is first added to the element
+    setMenuHeight(height);
+  }
 
   function DropdownItem(props){
 
     return (
-      // pass in props to control the link text
+      // pass in props to control the link text; onClick looks for a prop that's passed down from the parent component and confirms prop exists before calling setActiveMenu function
       <a href='#' className='dropdown-menu-item' onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
         {/* if left or right Icon prop is passed, then it will be rendered. If blank, nothing happens. Text is taken as a child. */}
         <span className='icon-button left-icon'>{props.leftIcon}</span>
@@ -58,23 +74,25 @@ function DropdownMenu() {
   }
 
   return(
-  <div className='dropdown'>
+  <div className='dropdown' style={{height: menuHeight}}>
     {/* Looks for a prop of in={} when true will animate the children into the UI. unmountOnExit removes the children when not active with a timeout that defines the duration of the animation. Adds or removes classes based on the state of the animation and CSS handles the animation. */}
     <CSSTransition 
     in={activeMenu === 'main'} 
     unmountOnExit 
     timeout={500}
-    classNames="menu-default">
+    classNames="menu-default"
+    // element is provided as an argument which allows use of function as the prop value and the height is pulled from the style attribute which pulls that from the state. 
+    onEnter={calcHeight}>
       <div className='menu'>
         <DropdownItem 
           // rightIcon={<ChevRight />}
           >Menu Item
         </DropdownItem>
         <DropdownItem 
-          // leftIcon={<ItemIcon />}
+          leftIcon={<SeasonsIcon />}
           rightIcon={<ChevRight />}
-          goToMenu="menu-default"
-          >
+          goToMenu="main"
+          >Seasons
         </DropdownItem>
       </div>
     </CSSTransition>
@@ -86,14 +104,28 @@ function DropdownMenu() {
     classNames="menu-secondary">
       <div className='menu'>
         <DropdownItem 
-          // rightIcon={<ChevRight />}
-          >Settings
+          leftIcon={<ChevLeft />}
+          >Return
         </DropdownItem>
         <DropdownItem 
-          leftIcon={<TreeIcon />}
-          rightIcon={<ChevRight />}
-          goToMenu="seasons-menu"
-          >
+          leftIcon={<SpringSeedling />}          
+          to="/spring"
+          >Spring
+        </DropdownItem>
+        <DropdownItem 
+          leftIcon={<SummerUmbrella />}          
+          to="/summer"
+          >Summer
+        </DropdownItem>
+        <DropdownItem 
+          leftIcon={<FallLeaf />}          
+          to="/autumn"
+          >Autumn
+        </DropdownItem>
+        <DropdownItem 
+          leftIcon={<WinterSnowflake />}          
+          to="/winter"
+          >Winter
         </DropdownItem>
       </div>
     </CSSTransition>
